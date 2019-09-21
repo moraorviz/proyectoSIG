6b4d32046aaa37db3772728db788b129696158ba
Mario Moraño Orviz <mariomoranoorviz@Marios-MacBook-Pro.local>
2019-09-21 18:26:09 +0200

    terminado servicio wms

--- script.js
+++ script.js
@@ -10,11 +10,39 @@ mainArea.mapa = document.querySelector(".mapa");
 mainArea.mapa.style.overflow = "hidden";
 mainArea.mapa.style.float = "left";
 mainArea.botones = Array.from(document.querySelectorAll(".boton"));
-mainArea.botones.forEach(function(item){
-    item.addEventListener('click', handleBtn);
+mainArea.botones.forEach(function(item) {
+    item.addEventListener("click", handleBtn);
     item.addEventListener('mouseover', changeBackground);
     item.addEventListener('mouseout', restoreBackground);
-})
+});
+const WMSURL = "http://mapadefosas.mjusticia.es/geoserver/wms?";
+const WMSLAYERS = "INTERVENIDA,NOINTERV";
+
+let CapaWMS = function(coord, zoom) {
+    let proj = map.getProjection();
+    let zfactor = Math.pow(2, zoom);
+    let top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor) ); 
+    let bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor)); 
+    let bbox = top.lng() + "," + bot.lat() + "," + bot.lng() + "," + top.lat();
+    let serviceURL = WMSURL + "SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG%3A4326&WIDTH=256&HEIGHT=256&FORMAT=image/png&TRANSPARENT=TRUE";
+    serviceURL += "&LAYERS="+ WMSLAYERS;
+    serviceURL += "&BBOX=" + bbox;
+    return serviceURL;
+}
+
+function enableButtons() {
+    mainArea.botones.forEach(function(item) {
+        item.addEventListener("click", handleBtn);
+    })
+}
+
+function disableOthers(name) {
+    mainArea.botones.forEach(function(item) {
+        if (!item.classList.contains(name)) {
+            item.removeEventListener("click", handleBtn);
+        }
+    });
+}
 
 function changeBackground(e) {
     e.target.style.backgroundColor = "grey";
@@ -31,18 +59,40 @@ function handleBtn(e){
     if (e.target.classList.contains("accion1")) {
         if (!e.target.classList.contains("pulsado")) {
             console.log("Se lanza la accion 1.");
+            e.target.classList.add("pulsado");
+            disableOthers("accion1")
             mostrarRuta();
         } else {
-        console.log("Ya esta pulsado.");
-        removerRuta();
+            console.log("Ya esta pulsado el boton 1.");
+            e.target.classList.remove("pulsado");
+            enableButtons();
+            removerRuta();
         }
-    }
-
-    if (e.target.classList.contains("pulsado")) {
-        e.target.classList.remove("pulsado");
+    } else if (e.target.classList.contains("accion2")) {
+        if (!e.target.classList.contains("pulsado")) {
+            console.log("Se lanza la accion 2.");
+            e.target.classList.add("pulsado");
+            disableOthers("accion2");
+            mostrarWMS();
+        } else {
+            console.log("Ya esta pulsado el boton 2.");
+            e.target.classList.remove("pulsado");
+            enableButtons();
+            removerWMS();
+        }
     } else {
-        e.target.classList.add("pulsado");
-    }
+        if (!e.target.classList.contains("pulsado")) {
+            console.log("Se lanza la accion 3.");
+            e.target.classList.add("pulsado");
+            disableOthers("accion3");
+            mostrarServicioMaps();
+        } else {
+            console.log("Ya esta pulsado el boton 3.");
+            e.target.classList.remove("pulsado");
+            enableButtons();
+            removerServicioMaps();
+        }
+    } 
 }
 
 function mostrarRuta() {
@@ -54,6 +104,29 @@ function removerRuta() {
     kmlLayer.setMap(null);
     kmlLayer = null;
     map.setZoom(7);
+}
+
+function mostrarWMS() {
+    console.log("Mostrando capa WMS.");
+    let overlayOptions = {
+        getTileUrl: CapaWMS,
+        tileSize: new google.maps.Size(256,256)
+    }
+    var overlayWMS = new google.maps.ImageMapType(overlayOptions);
+    map.overlayMapTypes.push(overlayWMS);
+}
+
+function removerWMS() {
+    console.log("Removiendo capa WMS");
+    map.overlayMapTypes.clear();
+}
+
+function mostrarServicioMaps() {
+    console.log("Mostrando servicio maps.");
+}
+
+function removerServicioMaps() {
+    console.log("Removiendo servicio maps.");
 }
 
 function initMap() {
